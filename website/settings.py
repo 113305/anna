f8d0fb35fe4c57e24cdcc321fafe083f333c2e6a
MINSEON <113305@naver.com>
2016-01-24 15:09:11 +0900

    change settings

--- website/settings.py
+++ website/settings.py
@@ -140,7 +140,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
